e30ea2962339aab76005c2c1bea79e3f62b34e91
Losses Don <losses.don@gmail.com>
2015-03-29 20:03:47 +0800

    support hash tag now

--- GreenCall/HOOK-AFTER_POST.php
+++ GreenCall/HOOK-AFTER_POST.php
@@ -2,9 +2,20 @@
 
 global $data;
 
-$pattern = '/&gt;&gt;(\S*)\s/i';
-$replacement = '<span class="green_call">&gt;&gt;${1}</span>';
+$regex = [
+    [
+        'pattern' => '/&gt;&gt;(\S*)(\s*)/i',
+        'replacement' => '<span class="green_call">&gt;&gt;${1}</span>${2}'
+    ],
+    [
+        'pattern' => '/#(\S*)(\s*)/i',
+        'replacement' => '<a class="green_call_tag" href="#/search/${1}">#${1}</a>${2}'
+    ]
+];
+
 
 for ($i = 0; $i < count($data); $i++) {
-    $data[$i]['content'] = preg_replace($pattern, $replacement, $data[$i]['content']);
+    for ($j = 0; $j < count($regex); $j++) {
+        $data[$i]['content'] = preg_replace($regex[$j]['pattern'], $regex[$j]['replacement'], $data[$i]['content']);
+    }
 }
