4e6b399863679932f89141c23aeb09ff413b8570
Losses Don <losses.don@gmail.com>
2015-04-05 16:58:23 +0800

    support custom token

--- AmnesicSession/HOOK-BEFORE_START.php
+++ AmnesicSession/HOOK-BEFORE_START.php
@@ -6,6 +6,8 @@
  * Time: 6:59 PM
  */
 
+global $database;
+
 require_once('AmnesicSession.php');
 
 $amnesic_session = new AmnesicSession();
@@ -18,3 +20,11 @@ session_set_save_handler(
     array($amnesic_session, 'destroy'),
     array($amnesic_session, 'gc')
 );
+if (isset($_POST['token'])) {
+    $session_list = $database->select('amnesicsession', 'id', [
+        'session_id[=]' => $_POST['token']
+    ]);
+
+    if (count($session_list) != 0)
+        session_id($_POST['token']);
+}
