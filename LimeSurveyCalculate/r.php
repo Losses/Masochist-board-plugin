d15f675d3eba4fd3f0f4f6c265e7d7ed4daa489f
Losses Don <losses.don@gmail.com>
2015-04-08 19:46:02 +0800

    bugfix

--- LimeSurveyCalculate/r.php
+++ LimeSurveyCalculate/r.php
@@ -28,18 +28,36 @@ class r
     {
         global $current_time;
 
-        $sheets = '"' . implode('","', func_get_args()) . '"';
-        $where_query = "WHERE `sheet` IN ($sheets)";
+        //$sheets = '"' . implode('","', func_get_args()) . '"';
+        //$where_query = "WHERE `sheet` IN ($sheets)";
+
+        $sheets = func_get_args();
 
         $random_file_name = md5($current_time . rand(0, 500));
+        /*
+                $result = $this->database->query("SELECT *
+                                                  FROM `limesurvey`
+                                                  $where_query
+                                                  INTO OUTFILE '$this->tmp_dir$random_file_name.csv'
+                                                  FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"'
+                                                  LINES TERMINATED BY '\\n';
+                                                  SELECT 'DONE'");
+                                        //->fetchAll();
 
-        $result = $this->database->query("SELECT *
-                                          FROM `limesurvey`
-                                          $where_query
-                                          INTO OUTFILE '$this->tmp_dir$random_file_name.csv'
-                                          FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"'
-                                          LINES TERMINATED BY '\\n'")
-                                ->fetchAll();
+        */
+        $result = $this->database->select('limesurvey', '*', [
+            'sheet' => $sheets
+        ]);
+
+        $file_steam = fopen("$this->tmp_dir$random_file_name.csv", 'w');
+
+        fputcsv($file_steam, ['id', 'sheet', 'type', 'key', 'value', 'user']);
+
+        foreach ($result as $fields) {
+            fputcsv($file_steam, $fields);
+        }
+
+        fclose($file_steam);
 
         return "$this->tmp_dir$random_file_name.csv";
     }
@@ -51,7 +69,7 @@ class r
         $r_arguments = '';
 
         for ($i = 1; $i < count($arguments); $i++) {
-            $r_arguments .= $arguments[$i];
+            $r_arguments .= escapeshellcmd($arguments[$i]);
 
             if ($i = count($arguments) - 1)
                 break;
@@ -59,7 +77,9 @@ class r
             $r_arguments .= ',';
         }
 
-        exec("$this->location $file_name $r_arguments");
+        exec("$this->location $file_name $r_arguments", $result);
+        var_dump($result);
+
         print_r("$this->location $this->dir$file_name $r_arguments");
     }
 }
